8315686311be9462d0fd6340a681ea1e6cbe6bc6
dav100ide <oreglio.dev@gmail.com>
2022-11-08 16:50:27 +0100

    hotwell modo corretto per selezionare i likeBtns ?

--- main.js
+++ main.js
@@ -21,17 +21,17 @@ const posts = [
       likes: 120,
       created: '2021-09-03',
    },
-   //    {
-   //       id: 3,
-   //       content: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
-   //       media: 'https://unsplash.it/600/400?image=234',
-   //       author: {
-   //          name: 'Chiara Passaro',
-   //          image: 'https://unsplash.it/300/300?image=20',
-   //       },
-   //       likes: 78,
-   //       created: '2021-05-15',
-   //    },
+   {
+      id: 3,
+      content: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+      media: 'https://unsplash.it/600/400?image=234',
+      author: {
+         name: 'Chiara Passaro',
+         image: 'https://unsplash.it/300/300?image=20',
+      },
+      likes: 78,
+      created: '2021-05-15',
+   },
    {
       id: 4,
       content: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
@@ -43,30 +43,46 @@ const posts = [
       likes: 56,
       created: '2021-04-03',
    },
-   //    {
-   //       id: 5,
-   //       content: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
-   //       media: 'https://unsplash.it/600/400?image=534',
-   //       author: {
-   //          name: 'Alessandro Sainato',
-   //          image: 'https://unsplash.it/300/300?image=29',
-   //       },
-   //       likes: 95,
-   //       created: '2021-03-05',
-   //    },
+   {
+      id: 5,
+      content: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+      media: 'https://unsplash.it/600/400?image=534',
+      author: {
+         name: 'Alessandro Sainato',
+         image: 'https://unsplash.it/300/300?image=29',
+      },
+      likes: 95,
+      created: '2021-03-05',
+   },
 ];
-/*=======================
-    handlebar
-=========================*/
+/*=========================================
+    functions
+=============================================*/
 
+/*=========================================
+    inserimento dom con handlebar
+===========================================*/
 const container = document.getElementById('container');
 
 for (let i = 0; i < posts.length; i++) {
    const post = posts[i];
-   //handelbars template
+   //handelbars contenuto card
    const source = document.getElementById('template-post').innerHTML;
    const template = Handlebars.compile(source);
    const context = post;
    const card = template(context);
    container.innerHTML += card;
+   //seleziono il bottone like
+}
+
+/*=========================================
+    main
+=============================================*/
+
+const likeBtns = document.querySelectorAll('.js-like-button');
+for (let i = 0; i < likeBtns.length; i++) {
+   const likeBtn = likeBtns[i];
+   likeBtn.addEventListener('click', function () {
+      alert('cciao');
+   });
 }
