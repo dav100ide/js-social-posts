f60c01af408bc344e84e8cf4fa8618b049ba5545
dav100ide <oreglio.dev@gmail.com>
2022-11-08 23:49:16 +0100

    content realistico

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const posts = [
    {
       id: 1,
-      content: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+      content: 'Pioveee libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
       media: 'https://unsplash.it/600/300?image=171',
       author: {
          name: 'Phil Mangione',
@@ -12,7 +12,7 @@ const posts = [
    },
    {
       id: 2,
-      content: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+      content: 'Campo agricoloo libero ipsa quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
       media: 'https://unsplash.it/600/400?image=112',
       author: {
          name: 'Sofia Perlari',
@@ -23,7 +23,7 @@ const posts = [
    },
    {
       id: 3,
-      content: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+      content: 'Parigi libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
       media: 'https://unsplash.it/600/400?image=234',
       author: {
          name: 'Chiara Passaro',
@@ -34,7 +34,7 @@ const posts = [
    },
    {
       id: 4,
-      content: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+      content: 'Libro isièx ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
       media: 'https://unsplash.it/600/400?image=24',
       author: {
          name: 'Luca Formicola',
@@ -45,7 +45,7 @@ const posts = [
    },
    {
       id: 5,
-      content: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+      content: 'Sedia arancione ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
       media: 'https://unsplash.it/600/400?image=534',
       author: {
          name: 'Alessandro Sainato',
