5640361eb62a01fa0e3cc61166dd168627e7a4dc
dav100ide <oreglio.dev@gmail.com>
2022-11-08 16:27:56 +0100

    cancella tag html inutili

--- main.js
+++ main.js
@@ -21,17 +21,17 @@ const posts = [
       likes: 120,
       created: '2021-09-03',
    },
-   {
-      id: 3,
-      content: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
-      media: 'https://unsplash.it/600/400?image=234',
-      author: {
-         name: 'Chiara Passaro',
-         image: 'https://unsplash.it/300/300?image=20',
-      },
-      likes: 78,
-      created: '2021-05-15',
-   },
+   //    {
+   //       id: 3,
+   //       content: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+   //       media: 'https://unsplash.it/600/400?image=234',
+   //       author: {
+   //          name: 'Chiara Passaro',
+   //          image: 'https://unsplash.it/300/300?image=20',
+   //       },
+   //       likes: 78,
+   //       created: '2021-05-15',
+   //    },
    {
       id: 4,
       content: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
@@ -43,17 +43,17 @@ const posts = [
       likes: 56,
       created: '2021-04-03',
    },
-   {
-      id: 5,
-      content: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
-      media: 'https://unsplash.it/600/400?image=534',
-      author: {
-         name: 'Alessandro Sainato',
-         image: 'https://unsplash.it/300/300?image=29',
-      },
-      likes: 95,
-      created: '2021-03-05',
-   },
+   //    {
+   //       id: 5,
+   //       content: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+   //       media: 'https://unsplash.it/600/400?image=534',
+   //       author: {
+   //          name: 'Alessandro Sainato',
+   //          image: 'https://unsplash.it/300/300?image=29',
+   //       },
+   //       likes: 95,
+   //       created: '2021-03-05',
+   //    },
 ];
 /*=======================
     handlebar
@@ -63,10 +63,10 @@ const container = document.getElementById('container');
 
 for (let i = 0; i < posts.length; i++) {
    const post = posts[i];
-
+   //handelbars template
    const source = document.getElementById('template-post').innerHTML;
    const template = Handlebars.compile(source);
-   let context = post;
+   const context = post;
    const card = template(context);
    container.innerHTML += card;
 }
